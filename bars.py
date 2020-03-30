1cd988c633baac6f996f823041e36f58df323548
favour-zyliuyyqx <442855532@qq.com>
2020-03-30 23:46:37 +0800

    bars

---
+++ bars.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Bars module
+============
+"""
+def starbar(num):
+    """
+    :arg num:
+    """
+    print('*' * num)
+
+def hashbar(num):
+    """
+    :arg num:
+    """
+    print('#' * num)
+
+def simplebar(num):
+    """
+    :arg num:
+    """
+    print('-' * num)
